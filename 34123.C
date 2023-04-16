09e6a88d3b5e6a1223f0e91ab4fa39c1bd3f14de
dhruvanmaniya <130692982+dhruvanmaniya@users.noreply.github.com>
2023-04-16 15:01:19 +0530

    Add files via upload

---
+++ 34123.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+  int a,b,c,e,f,g,h;
+  clrscr();
+  a=3;
+  b=7;
+  c=a+b;
+  printf("addition of %d and %d is %d\n",a,b,c);
+  e=b*a;
+  printf("multipication of %d and %d is %d\n",a,b,e);
+  f=b/a;
+  printf("division of %d and %d is %d\n",a,b,f);
+  g=b-a;
+  printf("substraction of %d and %d is %d\n",a,b,g);
+  h=a%b;
+  printf("percentage of %d and %d is %d\n",a,b,h);
+
+
+  getch();
+}
